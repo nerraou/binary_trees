2baf52b56b27784cbbfa30f198899290d8ba5a72
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-05 16:57:17 +0100

    fix count height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,10 +19,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
 		if (left_height > right_height)
-			return (left_height + 1);
-		return (right_height + 1);
+			return (left_height);
+		return (right_height);
 	}
 }
