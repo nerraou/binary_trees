5ac13304bbeb1743bd3ae1684bf76b597a2150bb
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-05 17:15:30 +0100

    count tree node

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - count a node size
+ *
+ * @tree: Pointer to the tree
+ *
+ * Return: the node size
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		left_size = binary_tree_size(tree->left);
+		right_size = binary_tree_size(tree->right);
+		return (left_size + right_size + 1);
+	}
+}
