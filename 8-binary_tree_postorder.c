09dcc4d95aba506eab2fff6472823614664fa896
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-05 11:43:29 +0100

    postorder and inorder traversal implementation

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - binary tree postorder
+ *
+ * @tree: Pointer to the tree
+ * @func: function to apply on tree
+ *
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
